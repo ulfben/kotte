fa9f2b7d5bb2978eb7b9034cd43bafa17723fe6a
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2026-07-17 20:48:52 +0200

    remove x86 config simplify .gitignore add vcxproj.filters for file organization

--- kotte/src/main.cpp
+++ kotte/src/main.cpp
@@ -2,24 +2,19 @@
 #include <raylib.h>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]]  char** argv){
+    
     InitWindow(920, 920, "kotte");
     InitAudioDevice();
+    
     SetExitKey(KEY_ESCAPE);
 
-    
-
     while(!WindowShouldClose()){
-    
-        ClearBackground(Color{0x11, 0x22, 0x33, 0xFF});
         BeginDrawing();
-
-        
-
+        ClearBackground(Color{0x11, 0x22, 0x33, 0xFF});
         DrawFPS(GetScreenWidth() - 100, 2);
         EndDrawing();
     }
 
-    
     CloseAudioDevice();
     CloseWindow();
 
